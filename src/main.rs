daa2d9b7e532b6d75d288a214cefb5f19b684b7b
Maxime Bourque-Fortin <maxime.bourquefortin@gmail.com>
2023-05-11 21:59:58 -0400

    If let

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let value = Some(6);
+    if let Some(x) = value {
+        println!("Value is {}", x);
+    }
+    else {
+        println!("No value")
+    }
 }
