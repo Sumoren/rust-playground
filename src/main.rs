dd3aaf036d22747453c645ec291bd65dcb743a19
Maxime Bourque-Fortin <maxime.bourquefortin@gmail.com>
2023-08-13 11:05:42 -0400

    Trait private impl test

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,27 @@ fn main() {
     else {
         println!("No value")
     }
+
+    use my_mod::MyTrait;
+    let obj = my_mod::create_obj();
+    println!("Value is {}", obj.do_stuff());
+}
+
+mod my_mod {
+    pub trait MyTrait {
+        fn do_stuff(&self) -> u32;
+    }
+
+    struct MyPrivateType {
+    }
+
+    impl MyTrait for MyPrivateType {
+        fn do_stuff(&self) -> u32 {
+            return 42;
+        }
+    }
+
+    pub fn create_obj() -> impl MyTrait {
+        MyPrivateType{}
+    }
 }
