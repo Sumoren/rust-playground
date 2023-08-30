de9a50957934d83a76ef9bea1a1b5e16571cb9fe
Maxime Bourque-Fortin <maxime.bourquefortin@gmail.com>
2023-08-30 17:57:17 -0400

    trait impl/dyn test

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,59 @@ fn main() {
     use my_mod::MyTrait;
     let obj = my_mod::create_obj();
     println!("Value is {}", obj.do_stuff());
+    print_type_of(&obj);
+    so_action(obj);
+
+    let obj2 = my_mod::MyOtherType{};
+    so_action(obj2);
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn so_action(obj : impl my_mod::MyTrait) {
+    println!("Value is {} in so_action", obj.do_stuff());
+
+    fn f() {}
+    print_type_of(&f);
+}
+
+
+fn test_dyn(obj : &dyn my_mod::MyTrait) {
+
+}
 mod my_mod {
     pub trait MyTrait {
         fn do_stuff(&self) -> u32;
     }
 
-    struct MyPrivateType {
-    }
+    struct MyPrivateType;
+
+    pub struct MyOtherType;
 
     impl MyTrait for MyPrivateType {
         fn do_stuff(&self) -> u32 {
-            return 42;
+            42
+        }
+    }
+
+    impl MyTrait for MyOtherType {
+        fn do_stuff(&self) -> u32 {
+            100
         }
     }
 
     pub fn create_obj() -> impl MyTrait {
         MyPrivateType{}
+    }
+
+    pub fn create_obj_2(b: bool) -> Box<dyn MyTrait> {
+        if b {
+            return Box::new(MyPrivateType{});
+        }
+        else {
+            return Box::new(MyOtherType{});
+        }
     }
 }
